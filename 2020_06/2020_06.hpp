f05228379a0fa76fba56ecbcd7856f019b5c027c
0xffset <rene.gottschalk02@gmail.com>
2020-12-06 07:01:50 +0100

    Day 6

---
+++ 2020_06/2020_06.hpp
@@ -0,0 +1,14 @@
+//
+// Created by u-0xffset on 06.12.20.
+//
+
+#ifndef ADVENT_OF_CODE_2020_06_HPP
+#define ADVENT_OF_CODE_2020_06_HPP
+
+namespace y2020 {
+    int day6part1();
+
+    int day6part2();
+}
+
+#endif //ADVENT_OF_CODE_2020_06_HPP
--- main.cpp
+++ main.cpp
@@ -7,6 +7,7 @@
 #include "2020_03/2020_03.hpp"
 #include "2020_04/2020_04.hpp"
 #include "2020_05/2020_05.hpp"
+#include "2020_06/2020_06.hpp"
 
 
 int main() {
@@ -27,6 +28,8 @@ int main() {
     std::cout << "2020  4th December Part 2: " << y2020::day4part2() << std::endl;
     std::cout << "2020  5th December Part 1: " << y2020::day5part1() << std::endl;
     std::cout << "2020  5th December Part 2: " << y2020::day5part2() << std::endl;
+    std::cout << "2020  6th December Part 1: " << y2020::day6part1() << std::endl;
+    std::cout << "2020  6th December Part 2: " << y2020::day6part2() << std::endl;
 
     return 0;
 }
